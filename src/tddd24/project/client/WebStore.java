ac8f3337a31d2c4c2d602ddc1053e9414b37a3df
Filip Källström <filip.kallstrom2@gmail.com>
2013-02-24 13:37:43 +0100

    added needed files to start to project

---
+++ src/tddd24/project/client/WebStore.java
@@ -0,0 +1,11 @@
+package tddd24.project.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class WebStore implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+	}
+
+}
