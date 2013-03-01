33a692ba9219da3343da3238e20b77e96bb23843
Filip Källström <filip.kallstrom@gmail.com>
2013-03-01 16:48:11 +0100

    WIP: test av flowpanel

--- src/tddd24/project/client/WebStore.java
+++ src/tddd24/project/client/WebStore.java
@@ -1,10 +1,34 @@
 package tddd24.project.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 public class WebStore implements EntryPoint {
 
+	FlowPanel flowPanel = new FlowPanel();
+	FlowPanel horPanel;
+	Label label;
+	Button button;
+	
 	@Override
 	public void onModuleLoad() {
-	}	
+		flowPanel.setSize("500px", "500px");
+		flowPanel.addStyleName("somestyle");
+		for(int i = 0; i < 10; i++)
+			flowPanel.add(createProductPanel());
+		RootPanel.get("HomePageBody").add(flowPanel);
+	}
+	
+	private Widget createProductPanel(){
+		horPanel = new FlowPanel();
+		//horPanel.addStyleName("somestyle");
+//		horPanel.add(label);
+	//	horPanel.add(button);
+		return horPanel;
+	}
 }
