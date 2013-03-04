8cfefadcf44cf718688868ecd8876f379551e6c6
Filip Källström <filip.kallstrom@gmail.com>
2013-03-04 17:56:30 +0100

    login ui

--- src/tddd24/project/client/WebStore.java
+++ src/tddd24/project/client/WebStore.java
@@ -10,20 +10,26 @@ import tddd24.project.widgets.ShoppingCartWidget;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Position;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.layout.client.Layout;
+import com.google.gwt.layout.client.Layout.Alignment;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Tree;
 import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class WebStore implements EntryPoint {
 
-	private HorizontalPanel topPanel = new HorizontalPanel();
+	private LayoutPanel topPanel = new LayoutPanel();
 	private HorizontalPanel bodyPanel = new HorizontalPanel();
 	private VerticalPanel categoryPanel = new VerticalPanel();
 	private Tree tree = new Tree(); // Tree that shows the categories
@@ -33,6 +39,11 @@ public class WebStore implements EntryPoint {
 	private FlowPanel mainPanel = new FlowPanel();
 	
 	private Label headerLabel = new Label("WebStore");
+	private TextBox userNameBox = new TextBox();
+	private PasswordTextBox passwordBox = new PasswordTextBox();
+	
+	private Label userNameLabel = new Label("User Name:");
+	private Label passwordLabel = new Label("Password:");
 
 	//DND
 	private ProductDragController productDragController;
@@ -42,8 +53,27 @@ public class WebStore implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
+		//header layout
 		headerLabel.setStyleName("headerLabel");
 		topPanel.add(headerLabel);
+		topPanel.add(userNameBox);
+		topPanel.add(passwordBox);
+		topPanel.add(userNameLabel);
+		topPanel.add(passwordLabel);
+//		userNameBox.setStyleName("userNameBox");
+//		userNameLabel.setStylePrimaryName("userNameLabel");
+//		passwordBox.setStyleName("passwordBox");
+//		passwordLabel.setStyleName("passwordLabel");
+		topPanel.setWidgetTopBottom(headerLabel, 0, Unit.PX, 0, Unit.PX);
+		topPanel.setWidgetRightWidth(userNameBox, 160.0, Unit.PX, 150.0, Unit.PX);
+		topPanel.setWidgetTopHeight(userNameBox, 20.0, Unit.PX, 30.0, Unit.PX);
+		topPanel.setWidgetRightWidth(passwordBox, 0.0, Unit.PX, 150.0, Unit.PX);
+		topPanel.setWidgetTopHeight(passwordBox, 20.0, Unit.PX, 30.0, Unit.PX);
+		topPanel.setWidgetRightWidth(userNameLabel, 160.0, Unit.PX, 147.0, Unit.PX);
+		topPanel.setWidgetTopHeight(userNameLabel, 0.0, Unit.PX, 30.0, Unit.PX);
+		topPanel.setWidgetRightWidth(passwordLabel, 0.0, Unit.PX, 147.0, Unit.PX);
+		topPanel.setWidgetTopHeight(passwordLabel, 0.0, Unit.PX, 30.0, Unit.PX);
+		
 		
 		bodyPanel.setBorderWidth(1);
 
